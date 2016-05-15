aa0da45c63f93e89e8d43e523ad02c7d15db234e
Stephanie Wilkinson <what.happens@gmail.com>
2016-05-14 17:02:12 -0700

    adds move NE function

--- assets/js/application.js
+++ assets/js/application.js
@@ -8,6 +8,13 @@
 //Functions
 ////////
 
+function nextChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) + 1);
+}
+
+function prevChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) - 1);
+}
 
 var allRowsMap = function() {
     return $.trim($(this).text());
@@ -15,13 +22,27 @@ var allRowsMap = function() {
 
 var initializeGame = function(x) {
   //sets white marbles all with text 1
-  $('#i5, #i6, #i7, #i8, #i9, #h4, #h5, #h6, #h7, #h8, #h9, #g5, #g6, #g7').text("1");
-  $('#i5, #i6, #i7, #i8, #i9, #h4, #h5, #h6, #h7, #h8, #h9, #g5, #g6, #g7').css("backgroundColor", "#ff358b");
+  var initWhiteMarblesIds = '#i5, #i6, #i7, #i8, #i9, #h4, #h5, #h6, #h7, #h8, #h9, #g5, #g6, #g7'
+  $(initWhiteMarblesIds).text("1");
+  $(initWhiteMarblesIds).css("backgroundColor", "#ff358b");
   //sets black marbles all with text -1
-  // $(selector).text(content)
+  var initBlackMarblesIds = '#a1, #a2, #a3, #a4, #a5, #b1, #b2, #b3, #b4, #b5, #b6, #c3, #c4, #c5'
+  $(initBlackMarblesIds).text("-1");
+  $(initBlackMarblesIds).css("backgroundColor", "#000");
   var allRows = $('.row').map(allRowsMap);
   console.log(allRows);
 }
+
+var moveNE = function(marble) {
+  //what are the coordinates of this marble
+  // add one to x
+  var x = nextChar($(marble).data('x'));
+  // add one to y
+  var y = $(marble).data('y') + 1;
+  return '#' + x + y;
+}
+
+console.log(moveNE('#a1'))  ;
 
 var allRows = $('.row').map(allRowsMap);
 console.log(allRows);
