4b2a3da2c3f3d3944834047ecc42448061f49666
Stephanie Wilkinson <what.happens@gmail.com>
2016-05-16 15:09:04 -0700

    building out the board objects

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,63 +1,63 @@
-  {marble: null, w: 0, nw: null, ne: null, e: 2, se: 7, sw: 6},
-  {marble: null, w: 1, nw: null, ne: null, e: 3, se: 8, sw: 7},
-  {marble: null, w: 2, nw: null, ne: null, e: 4, se: 9, sw: 7},
-  {marble: null, w: 3, nw: null, ne: null, e: null, se: 9, sw: 10},
-  {marble: null, w: null, nw: null, ne: 0, e: 6, se: 11, sw: 12},
-  {marble: null, w: 5, nw: 0, ne: 1, e: 7, se: 13, sw: 12},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
-  {marble: null, w: 1, nw: null, ne: null, e: null, se: 6, sw: 5},
+  {marble: null, w: 0,    nw: null, ne: null, e: 2, se: 7, sw: 6},
+  {marble: null, w: 1,    nw: null, ne: null, e: 3, se: 8, sw: 7},
+  {marble: null, w: 2,    nw: null, ne: null, e: 4, se: 9, sw: 7},
+  {marble: null, w: 3,    nw: null, ne: null, e: null, se: 9, sw: 10},
+  {marble: null, w: null, nw: null, ne: 0,    e: 6, se: 12, sw: 11},
+  {marble: null, w: 5,    nw: 0,    ne: 1,    e: 7, se: 13, sw: 12},
+  {marble: null, w: 6,    nw: 1,    ne: 2,    e: 8, se: 14, sw: 13},
+  {marble: null, w: 7,    nw: 2,    ne: 3,    e: 9, se: 15, sw: 14},
+  {marble: null, w: 8,    nw: 3,    ne: 4,    e: null, se: 6, sw: 5},
+  {marble: null, w: 9,    nw: 4,    ne: null, e: , se: 6, sw: 5},
+  {marble: null, w: null, nw: null, ne: 5,    e: , se: 6, sw: 5},
+  {marble: null, w: 11,   nw: 5,    ne: 6,    e: , se: 6, sw: 5},
+  {marble: null, w: 12,   nw: 6,    ne: 7,    e: , se: 6, sw: 5},
+  {marble: null, w: 13,   nw: 7,    ne: 8,    e: , se: 6, sw: 5},
+  {marble: null, w: 14,   nw: 8,    ne: 9,    e: , se: 6, sw: 5},
+  {marble: null, w: 15,   nw: 9,    ne: 10,   e: , se: 6, sw: 5},
+  {marble: null, w: 16,   nw: 10,   ne: null, e: , se: 6, sw: 5},
+  {marble: null, w: null, nw: null, ne: 11,   e: , se: 6, sw: 5},
+  {marble: null, w: 18,   nw: 11,   ne: 12,   e: , se: 6, sw: 5},
+  {marble: null, w: 19,   nw: 12,   ne: 13,   e: , se: 6, sw: 5},
+  {marble: null, w: 20,   nw: 13,   ne: 14,   e: , se: 6, sw: 5},
+  {marble: null, w: 21,   nw: 14,   ne: 15,   e: , se: 6, sw: 5},
+  {marble: null, w: 22,   nw: 15,   ne: 16,   e: , se: 6, sw: 5},
+  {marble: null, w: 23,   nw: 16,   ne: 17,   e: , se: 6, sw: 5},
+  {marble: null, w: 24,   nw: 17,   ne: null, e: , se: 6, sw: 5},
+  {marble: null, w: null, nw: null, ne: 18,   e: , se: 6, sw: 5},
+  {marble: null, w: 26,   nw: 18,   ne: 19,   e: , se: 6, sw: 5},
+  {marble: null, w: 27,   nw: 19,   ne: 20,   e: , se: 6, sw: 5},
+  {marble: null, w: 28,   nw: 20,   ne: 21,   e: , se: 6, sw: 5},
+  {marble: null, w: 29,   nw: 21,   ne: 22,   e: , se: 6, sw: 5},
+  {marble: null, w: 30,   nw: 22,   ne: 23,   e: , se: 6, sw: 5},
+  {marble: null, w: 31,   nw: 23,   ne: 24,   e: , se: 6, sw: 5},
+  {marble: null, w: 32,   nw: 24,   ne: 25,   e: , se: 6, sw: 5},
+  {marble: null, w: 33,   nw: 25,   ne: null, e: , se: 6, sw: 5},
+  {marble: null, w: null, nw: 26,   ne: 27,   e: , se: 6, sw: 5},
+  {marble: null, w: 35,   nw: 27,   ne: 28,   e: , se: 6, sw: 5},
+  {marble: null, w: 36,   nw: 28,   ne: 29,   e: , se: 6, sw: 5},
+  {marble: null, w: 37,   nw: 29,   ne: 30,   e: , se: 6, sw: 5},
+  {marble: null, w: 38,   nw: 30,   ne: 31,   e: , se: 6, sw: 5},
+  {marble: null, w: 39,   nw: 31,   ne: 32,   e: , se: 6, sw: 5},
+  {marble: null, w: 40,   nw: 32,   ne: 33,   e: , se: 6, sw: 5},
+  {marble: null, w: 41,   nw: 33,   ne: 34,   e: , se: 6, sw: 5},
+  {marble: null, w: null, nw: 35,   ne: 36,   e: se: 6, sw: 5},
+  {marble: null, w: 43,   nw: 36,   ne: 37,   e: 45, se: 6, sw: 5},
+  {marble: null, w: 44,   nw: 37,   ne: 38,   e: 46, se: 6, sw: 5},
+  {marble: null, w: 45,   nw: 38,   ne: 39,   e: 47, se: 6, sw: 5},
+  {marble: null, w: 46,   nw: 39,   ne: 40,   e: 48, se: 6, sw: 5},
+  {marble: null, w: 47,   nw: 40,   ne: 41,   e: 49, se: 6, sw: 5},
+  {marble: null, w: 48,   nw: 41,   ne: 42,   e: null, se: 6, sw: 5},
+  {marble: null, w: null, nw: 43,   ne: 44,   e: 51, se: 6, sw: 5},
+  {marble: null, w: 50,   nw: 44,   ne: 45,   e: 52, se: 6, sw: 5},
+  {marble: null, w: 51,   nw: 45,   ne: 46,   e: 53, se: 6, sw: 5},
+  {marble: null, w: 52,   nw: 46,   ne: 47,   e: 54, se: 6, sw: 5},
+  {marble: null, w: 53,   nw: 47,   ne: 48,   e: 55, se: 6, sw: 5},
+  {marble: null, w: 54,   nw: 48,   ne: 49,   e: null, se: null, sw: 60},
+  {marble: null, w: null, nw: 50,   ne: 51,   e: 57, se: null, sw: null},
+  {marble: null, w: 56,   nw: 51,   ne: 52,   e: 58, se: null, sw: null},
+  {marble: null, w: 57,   nw: 52,   ne: 53,   e: 59, se: null, sw: null},
+  {marble: null, w: 58,   nw: 53,   ne: 54,   e: 60, se: null, sw: null},
+  {marble: null, w: 59,   nw: 54,   ne: 55,   e: null, se: null, sw: null},
   ];
 
 
@@ -68,13 +68,13 @@
 
 
 var board = [
-  {marble: null, w: null, nw: null, ne: null, e: 1, se: 5, sw: 6},
+  {marble: null, w: null,   nw: null,   ne: null,    e: 1, se: 5, sw: 6},
 
 
 
 var selectedMarbles = [];
 var whoseTurn = 1;
-var direction = "ne";
+var direction = " ne";
 
 ////////
 //Functions
@@ -85,7 +85,7 @@ function getAdjacent(index, dir) {
 }
 
 
-function nextChar(c) {
+function   nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 
@@ -111,8 +111,8 @@ var initializeGame = function(x) {
 }
 
 
-var moveNE = function(marble) {
-  var x = nextChar($(marble).data('x'));
+var move ne = function(marble) {
+  var x =   nextChar($(marble).data('x'));
   var y = $(marble).data('y') + 1;
   return '#' + x + y;
 }
@@ -142,13 +142,13 @@ var moveW = function(marble) {
 }
 
 var moveNW = function(marble) {
-  var x = nextChar($(marble).data('x'));
+  var x =   nextChar($(marble).data('x'));
   var y = $(marble).data('y');
   return '#' + x + y;
 }
 
 // console.log(moveSE('#a1'));
-// console.log(moveNE('#a1'));
+// console.log(move ne('#a1'));
 // console.log(moveE('#a1'));
 // console.log(moveSW('#a1'));
 // console.log(moveNW('#a1'));
@@ -160,7 +160,7 @@ var allRows = $('.row').map(allRowsMap);
 
 
 ////////
-//Event Listeners
+//Event Liste ners
 ////////
 
 
