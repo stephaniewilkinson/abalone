05262a9bcb044ae84d64c21fe5278e40742d86c6
Stephanie Wilkinson <what.happens@gmail.com>
2016-05-16 10:15:49 -0700

    add click listener

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,10 +3,13 @@
 //Data
 ////////
 
+var selectedMarbles = [];
+var whoseTurn = null;
 
 ////////
 //Functions
 ////////
+
 
 function nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
@@ -24,11 +27,11 @@ var initializeGame = function(x) {
   //sets white marbles all with text 1
   var initWhiteMarblesIds = '#i5, #i6, #i7, #i8, #i9, #h4, #h5, #h6, #h7, #h8, #h9, #g5, #g6, #g7'
   $(initWhiteMarblesIds).text("1");
-  $(initWhiteMarblesIds).css("backgroundColor", "#ff358b");
+  $(initWhiteMarblesIds).css("backgroundColor", "#bdf271");
   //sets black marbles all with text -1
   var initBlackMarblesIds = '#a1, #a2, #a3, #a4, #a5, #b1, #b2, #b3, #b4, #b5, #b6, #c3, #c4, #c5'
   $(initBlackMarblesIds).text("-1");
-  $(initBlackMarblesIds).css("backgroundColor", "#000");
+  $(initBlackMarblesIds).css("backgroundColor", "#01a2a6");
   var allRows = $('.row').map(allRowsMap);
   console.log(allRows);
 }
@@ -69,13 +72,35 @@ var moveNW = function(marble) {
   return '#' + x + y;
 }
 
-console.log(moveSE('#a1'));
-console.log(moveNE('#a1'));
-console.log(moveE('#a1'));
-console.log(moveSW('#a1'));
-console.log(moveNW('#a1'));
-console.log(moveW('#a1'));
+// console.log(moveSE('#a1'));
+// console.log(moveNE('#a1'));
+// console.log(moveE('#a1'));
+// console.log(moveSW('#a1'));
+// console.log(moveNW('#a1'));
+// console.log(moveW('#a1'));
 
 
 var allRows = $('.row').map(allRowsMap);
 console.log(allRows);
+
+
+////////
+//Event Listeners
+////////
+
+
+var addMarble = function(x) {
+  selectedMarbles.push(this);
+  $(this).css('background-color', '#95ab63');
+};
+
+$( ".cell").click(addMarble);
+
+
+
+
+console.log(selectedMarbles);
+
+
+
+
